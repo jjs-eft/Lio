858651ddfd469653be8eeb6bf20c0a79e508d5b7
jjs-eft <jjjs963@naver.com>
2021-11-01 19:57:27 +0900

    회원정보수정, 게시글삭제, 추천 UI 수정

--- src/main/java/com/linkinone/Lio/Controller/BoardController.java
+++ src/main/java/com/linkinone/Lio/Controller/BoardController.java
@@ -125,6 +125,17 @@ public class BoardController {
         return "redirect:/board-free.html/";
     }
 
+/*    // 자유게시판 검색
+    @GetMapping("/board/search")
+    public String search(@RequestParam(value="keyword") String keyword, Model model) {
+        List<BoardDto> boardDtoList = boardService.searchPosts(keyword);
+
+        model.addAttribute("boardList", boardDtoList);
+
+        return "board/list.html";
+    }*/
+
+
 
 
     //질문게시판
@@ -387,98 +398,7 @@ public class BoardController {
 
 }
 
-    /* 게시글 목록
-    @GetMapping("/board/list")
-    public String list(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
-        List<BoardDto> boardList = boardService.getBoardlist(pageNum);
-        Integer[] pageList = boardService.getPageList(pageNum);
 
-        model.addAttribute("boardList", boardList);
-        model.addAttribute("pageList", pageList);
-
-        return "board/list.html";
-    }
-
-    @GetMapping("/board/list2")
-    public String list2(Model model, @RequestParam(value="page", defaultValue = "1") Integer pageNum) {
-        List<BoardDto> boardList = boardService.getBoardlist2(pageNum);
-        Integer[] pageList = boardService.getPageList(pageNum);
-
-        model.addAttribute("boardList", boardList);
-        model.addAttribute("pageList", pageList);
-
-        return "board/list2.html";
-    }
-
-    @GetMapping("/post")
-    public String write(Model model, Authentication authentication) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        model.addAttribute("author",userDetails.getUsername());
-
-        return "board/write.html";
-    }
-
-    @PostMapping("/post")
-    public String write(BoardDto boardDto) {
-        boardService.savePost(boardDto);
-
-
-        return "redirect:/board/list";
-    }
-
-    
-    //게시글 상세 조회
-    @GetMapping("/post/{no}")
-    public String detail(@PathVariable("no") Long no, Model model, Authentication authentication) {
-        BoardDto boardDTO = boardService.getPost(no);
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        model.addAttribute("author",userDetails.getUsername());
-        model.addAttribute("boardDto", boardDTO);
-
-        return "board/detail.html";
-    }
-
-    //상세 조회에서의 댓글
-    @PostMapping("/commentwrite")
-    public String commentwrite(CommentDto commentDto) {
-        commentService.savePost(commentDto);
-
-        return "redirect:/board/list";
-    }
-
-    @GetMapping("/post/edit/{no}")
-    public String edit(@PathVariable("no") Long no, Model model, Authentication authentication) {
-        BoardDto boardDTO = boardService.getPost(no);
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        model.addAttribute("author",userDetails.getUsername());
-        model.addAttribute("boardDto", boardDTO);
-        return "board/update.html";
-    }
-
-    @PutMapping("/post/edit/{no}")
-    public String update(BoardDto boardDTO) {
-        boardService.savePost(boardDTO);
-
-        return "redirect:/board/list";
-    }
-
-    @DeleteMapping("/post/{no}")
-    public String delete(@PathVariable("no") Long no) {
-        boardService.deletePost(no);
-
-        return "redirect:/board/list";
-    }
-
-    @GetMapping("/board/search")
-    public String search(@RequestParam(value="keyword") String keyword, Model model) {
-        List<BoardDto> boardDtoList = boardService.searchPosts(keyword);
-
-        model.addAttribute("boardList", boardDtoList);
-
-        return "board/list.html";
-    }
-
- */
 
 
 
